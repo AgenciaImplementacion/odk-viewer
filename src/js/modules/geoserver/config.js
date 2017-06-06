85b766afaf1fd4643f982020e1f8b8f9ef26ae9d
Jorge Useche <naturalmentejorge@gmail.com>
2017-06-06 18:33:07 -0500

    Se actualiza configuración del servicio.

--- src/js/modules/geoserver/config.js
+++ src/js/modules/geoserver/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: 'http://geoportal.red:8080/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:datos&maxFeatures=50&outputFormat=application%2Fjson',
+  url: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:datos&maxFeatures=50&outputFormat=application%2Fjson',
   path_imagenes: 'http://odk.proadmintierra.info/odk/piloto/'
 };
 
