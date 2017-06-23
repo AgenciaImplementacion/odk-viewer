286cc91f1ada5215dc7ab96b478a2470f1eb1cf5
Jorge Useche <naturalmentejorge@gmail.com>
2017-06-22 22:54:00 -0500

    Se agrega capa de muestra sin popup

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,7 +1,12 @@
 const config = {
   url_datos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:datos&maxFeatures=50&outputFormat=application%2Fjson',
-  path_imagenes: 'http://odk.proadmintierra.info/odk/piloto/',
-  url_poligonos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:poligono&maxFeatures=50&outputFormat=application%2Fjson'
+  url_poligonos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:poligono&maxFeatures=50&outputFormat=application%2Fjson',
+  ortofoto: {
+    url: 'http://geo.red:8080/geoserver/ODK/wms',
+    layers: 'ODK:ortofoto_la_palma_208IB2',
+    format: 'image/png'
+  },
+  url_poligonos_muestreo: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:Muestreo&maxFeatures=50&outputFormat=application%2Fjson'
 };
 
 export default config;
--- src/js/modules/geoserver/cargar-servicio.js
+++ src/js/modules/geoserver/cargar-servicio.js
@@ -13,10 +13,64 @@ import LayerVector from 'ol/layer/vector';
 import Loadingstrategy from 'ol/loadingstrategy';
 import Tilegrid from 'ol/tilegrid';
 
+// para ortofoto
+import LayerTile from 'ol/layer/tile';
+import SourceTileWMS from 'ol/source/tilewms';
+
+// inicio ortofoto
+var orotofoLayer = new LayerTile({
+  title: 'Global Imagery',
+  source: new SourceTileWMS({
+    url: serverConfig.ortofoto.url,
+    params: {
+      'FORMAT': serverConfig.ortofoto.format,
+      'VERSION': '1.1.1',
+      tiled: true,
+      STYLES: '',
+      LAYERS: serverConfig.ortofoto.layers,
+    }
+  })
+});
+
+window.map.addLayer(orotofoLayer);
+// fin ortofoto
+
 // format used to parse WFS GetFeature responses
 var geojsonFormat = new GeoJSON();
 
-//segunda capa poligono
+
+// inicia capa poligonos de muestreo
+var serviceSource3 = new SourceVector({
+  loader: function(extent, resolution, projection) {
+    //console.log('bad extent', extent, resolution, projection);
+    var newExtent = window.map.getView().calculateExtent(window.map.getSize());
+    //console.log('new extent', newExtent);
+    //extent = newExtent.join(',');
+    extent = newExtent;
+
+    var url = serverConfig.url_poligonos_muestreo + '&srsname=EPSG:3857&bbox=' + extent.join(',') + ',EPSG:3857'
+    // use jsonp: false to prevent jQuery from adding the "callback"
+    // parameter to the URL
+    $.ajax({url: url, dataType: 'json', jsonp: false}).done(function(response) {
+      serviceSource3.addFeatures(geojsonFormat.readFeatures(response))
+    });
+  },
+  strategy: Loadingstrategy.tile(Tilegrid.createXYZ({maxZoom: 19}))
+});
+
+var serviceLayer3 = new LayerVector({
+  source: serviceSource3,
+  style: function(feature) {
+    //console.log('serviceLayer2', feature.getGeometry().getType());
+    return styles3[feature.getGeometry().getType()];
+  }
+});
+
+window.map.addLayer(serviceLayer3);
+
+// termina capa poligonos de muestreo
+
+//inicia segunda capa poligono
 
 var serviceSource2 = new SourceVector({
   loader: function(extent, resolution, projection) {
@@ -44,11 +98,13 @@ var serviceLayer2 = new LayerVector({
   }
 });
 
-//window.serviceLayer2 = serviceLayer2;
 window.map.addLayer(serviceLayer2);
 
 //termina segunda capa poligono
 
+
+
+// inicia capa puntos datos
 var serviceSource = new SourceVector({
   loader: function(extent, resolution, projection) {
     //console.log('bad extent', extent, resolution, projection);
@@ -76,166 +132,131 @@ var serviceLayer = new LayerVector({
 
 window.map.addLayer(serviceLayer);
 
+// termina capa puntos datos
+
 
 //style1
 var image = new Circle({
   radius: 5,
-  fill: new Fill({
-    color: 'rgba(223, 62, 62, 1)'
-  }),
-  stroke: new Stroke({
-    color: 'rgba(116, 43, 8, 0.45)',
-    width: 1
-  })
+  fill: new Fill({color: 'rgba(223, 62, 62, 1)'}),
+  stroke: new Stroke({color: 'rgba(116, 43, 8, 0.45)', width: 1})
 });
 
 var styles = {
-  'Point': new Style({
-    image: image
-  }),
+  'Point': new Style({image: image}),
   'LineString': new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 1
-    })
+    stroke: new Stroke({color: 'green', width: 1})
   }),
   'MultiLineString': new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 1
-    })
+    stroke: new Stroke({color: 'green', width: 1})
   }),
-  'MultiPoint': new Style({
-    image: image
-  }),
+  'MultiPoint': new Style({image: image}),
   'MultiPolygon': new Style({
-    stroke: new Stroke({
-      color: 'yellow',
-      width: 1
-    }),
-    fill: new Fill({
-      color: 'rgba(255, 255, 0, 0.1)'
-    })
+    stroke: new Stroke({color: 'yellow', width: 1}),
+    fill: new Fill({color: 'rgba(255, 255, 0, 0.1)'})
   }),
   'Polygon': new Style({
-    stroke: new Stroke({
-      color: 'blue',
-      lineDash: [4],
-      width: 3
-    }),
-    fill: new Fill({
-      color: 'rgba(0, 0, 255, 0.1)'
-    })
+    stroke: new Stroke({color: 'blue', lineDash: [4], width: 3}),
+    fill: new Fill({color: 'rgba(0, 0, 255, 0.1)'})
   }),
   'GeometryCollection': new Style({
-    stroke: new Stroke({
-      color: 'magenta',
-      width: 2
-    }),
-    fill: new Fill({
-      color: 'magenta'
-    }),
+    stroke: new Stroke({color: 'magenta', width: 2}),
+    fill: new Fill({color: 'magenta'}),
     image: new Circle({
       radius: 5,
       fill: null,
-      stroke: new Stroke({
-        color: 'magenta'
-      })
+      stroke: new Stroke({color: 'magenta'})
     })
   }),
   'Circle': new Style({
-    stroke: new Stroke({
-      color: 'red',
-      width: 2
-    }),
-    fill: new Fill({
-      color: 'rgba(255,0,0,0.2)'
-    })
+    stroke: new Stroke({color: 'red', width: 2}),
+    fill: new Fill({color: 'rgba(255,0,0,0.2)'})
   })
 };
 
 //poligono styles
 
 var styles2 = {
-  'Point': new Style({
-    image: image
-  }),
+  'Point': new Style({image: image}),
   'LineString': new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 1
-    })
+    stroke: new Stroke({color: 'green', width: 1})
   }),
   'MultiLineString': new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 1
-    })
+    stroke: new Stroke({color: 'green', width: 1})
   }),
-  'MultiPoint': new Style({
-    image: image
-  }),
+  'MultiPoint': new Style({image: image}),
   'MultiPolygon': new Style({
-    stroke: new Stroke({
-      color: 'rgb(58, 153, 0)',
-      width: 1
-    }),
-    fill: new Fill({
-      color: 'rgba(43, 255, 0, 0.1)'
-    })
+    stroke: new Stroke({color: 'rgb(58, 153, 0)', width: 1}),
+    fill: new Fill({color: 'rgba(43, 255, 0, 0.1)'})
   }),
   'Polygon': new Style({
-    stroke: new Stroke({
-      color: 'blue',
-      lineDash: [4],
-      width: 3
-    }),
-    fill: new Fill({
-      color: 'rgba(0, 0, 255, 0.1)'
-    })
+    stroke: new Stroke({color: 'blue', lineDash: [4], width: 3}),
+    fill: new Fill({color: 'rgba(0, 0, 255, 0.1)'})
   }),
   'GeometryCollection': new Style({
-    stroke: new Stroke({
-      color: 'magenta',
-      width: 2
-    }),
-    fill: new Fill({
-      color: 'magenta'
-    }),
+    stroke: new Stroke({color: 'magenta', width: 2}),
+    fill: new Fill({color: 'magenta'}),
     image: new Circle({
       radius: 10,
       fill: null,
-      stroke: new Stroke({
-        color: 'magenta'
-      })
+      stroke: new Stroke({color: 'magenta'})
     })
   }),
   'Circle': new Style({
-    stroke: new Stroke({
-      color: 'red',
-      width: 2
-    }),
-    fill: new Fill({
-      color: 'rgb(255,0,0)'
+    stroke: new Stroke({color: 'red', width: 2}),
+    fill: new Fill({color: 'rgb(255,0,0)'})
+  })
+};
+
+//poligono muestreo styles
+
+var styles3 = {
+  'Point': new Style({image: image}),
+  'LineString': new Style({
+    stroke: new Stroke({color: 'green', width: 1})
+  }),
+  'MultiLineString': new Style({
+    stroke: new Stroke({color: 'green', width: 1})
+  }),
+  'MultiPoint': new Style({image: image}),
+  'MultiPolygon': new Style({
+    stroke: new Stroke({color: 'rgb(153, 0, 0)', width: 1}),
+    fill: new Fill({color: 'rgba(255, 69, 0, 0.1)'})
+  }),
+  'Polygon': new Style({
+    stroke: new Stroke({color: 'blue', lineDash: [4], width: 3}),
+    fill: new Fill({color: 'rgba(0, 0, 255, 0.1)'})
+  }),
+  'GeometryCollection': new Style({
+    stroke: new Stroke({color: 'magenta', width: 2}),
+    fill: new Fill({color: 'magenta'}),
+    image: new Circle({
+      radius: 10,
+      fill: null,
+      stroke: new Stroke({color: 'magenta'})
     })
+  }),
+  'Circle': new Style({
+    stroke: new Stroke({color: 'red', width: 2}),
+    fill: new Fill({color: 'rgb(255,0,0)'})
   })
 };
 
 window.masInformacion = function(id) {
-   var url = 'https://ide.proadmintierra.info/odk/odk_p.php?id=' + id;
-   $.ajax({url: url}).done(function(response) {
-      console.log('response', response);
-      var properties = response[0];
+  var url = 'https://ide.proadmintierra.info/odk/odk_p.php?id=' + id;
+  $.ajax({url: url}).done(function(response) {
+    console.log('response', response);
+    var properties = response[0];
 
-      var content = document.getElementById('contenido-barra-lateral');
-      var contentHTML = '';
-      for (var property in properties) {
-        if (properties.hasOwnProperty(property)) {
-          if(properties[property]+'' !== 'null'){
-            contentHTML += '<span><b class="titulo-propiedades">' + property + ':</b><br/> ' + properties[property] + '</span><br/>';
-          }
+    var content = document.getElementById('contenido-barra-lateral');
+    var contentHTML = '';
+    for (var property in properties) {
+      if (properties.hasOwnProperty(property)) {
+        if (properties[property] + '' !== 'null') {
+          contentHTML += '<span><b class="titulo-propiedades">' + property + ':</b><br/> ' + properties[property] + '</span><br/>';
         }
       }
-      content.innerHTML = contentHTML;
-   });
+    }
+    content.innerHTML = contentHTML;
+  });
 }
