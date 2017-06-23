3605ad2781f19b96358b8542934ed7e2868568d4
Jorge Useche <naturalmentejorge@gmail.com>
2017-06-23 16:47:01 -0500

    Se hace accesible desde fuera.

--- src/conf/config.js
+++ src/conf/config.js
@@ -2,7 +2,7 @@ const config = {
   url_datos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:datos&maxFeatures=50&outputFormat=application%2Fjson',
   url_poligonos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:poligono&maxFeatures=50&outputFormat=application%2Fjson',
   ortofoto: {
-    url: 'http://geo.red:8080/geoserver/ODK/wms',
+    url: 'https://geo.proadmintierra.info/geoserver/ODK/wms',
     layers: 'ODK:ortofoto_la_palma_208IB2',
     format: 'image/png'
   },
