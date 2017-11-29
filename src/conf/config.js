aa724540cc17133ec0cf45ac0dc84033ff00fc55
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2017-11-29 16:59:00 -0500

    Se arreglan direcciones

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,12 +1,12 @@
 const config = {
-  url_datos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:datos&maxFeatures=50&outputFormat=application%2Fjson',
-  url_poligonos: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:poligono&maxFeatures=50&outputFormat=application%2Fjson',
+  url_datos: 'https://ide.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:datos&maxFeatures=50&outputFormat=application%2Fjson',
+  url_poligonos: 'https://ide.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:poligono&maxFeatures=50&outputFormat=application%2Fjson',
   ortofoto: {
-    url: 'https://geo.proadmintierra.info/geoserver/ODK/wms',
+    url: 'https://ide.proadmintierra.info/geoserver/ODK/wms',
     layers: 'ODK:ortofoto_la_palma_208IB2',
     format: 'image/png'
   },
-  url_poligonos_muestreo: 'https://geo.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:Muestreo&maxFeatures=50&outputFormat=application%2Fjson'
+  url_poligonos_muestreo: 'https://ide.proadmintierra.info/geoserver/ODK/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ODK:Muestreo&maxFeatures=50&outputFormat=application%2Fjson'
 };
 
 export default config;
